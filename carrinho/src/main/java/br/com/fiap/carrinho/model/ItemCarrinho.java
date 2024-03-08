de54d411ba0b75e09a921914c59e029fd1997eb0
caduh-uh <carlos.eduardo1801@hotmai.com>
2024-03-08 16:54:57 -0300

    add anotação

--- carrinho/src/main/java/br/com/fiap/carrinho/model/ItemCarrinho.java
+++ carrinho/src/main/java/br/com/fiap/carrinho/model/ItemCarrinho.java
@@ -1,6 +1,7 @@
 package br.com.fiap.carrinho.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Setter
 @Data
 @Entity
+@EqualsAndHashCode
 public class ItemCarrinho {
 
     @Id
